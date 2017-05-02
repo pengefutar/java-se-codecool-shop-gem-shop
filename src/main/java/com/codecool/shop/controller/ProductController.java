63380da7efa060c478febe197855d6653cb50fd3
pengefutar <siri.lukacs@gmail.com>
2017-05-02 16:25:31 +0200

    nested select added to index

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -19,13 +19,11 @@ import java.util.List;
 import java.util.Map;
 
 public class ProductController {
-
+    private static ProductDao productDataStore = ProductDaoMem.getInstance();
+    private static ProductCategoryDaoMem productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+    private static SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
 
     public static ModelAndView renderProducts(Request req, Response res) {
-        ProductDao productDataStore = ProductDaoMem.getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
-
         Map params = new HashMap<>();
         params.put("categories", productCategoryDataStore.getAll());
         params.put("suppliers", supplierDataStore.getAll());
@@ -34,13 +32,21 @@ public class ProductController {
     }
 
     public static ModelAndView renderByCategory(Request req, Response res) {
-        ProductDao productDataStore = ProductDaoMem.getInstance();
-        ProductCategoryDaoMem productCategoryDataStore = ProductCategoryDaoMem.getInstance();
         int searchedId = Integer.parseInt(req.params(":id"));
 
         Map params = new HashMap<>();
         params.put("categories", productCategoryDataStore.getAll());
+        params.put("suppliers", supplierDataStore.getAll());
         params.put("products", productDataStore.getBy(productCategoryDataStore.find(searchedId)));
+        return new ModelAndView(params, "product/index");
+    }
+
+    public static ModelAndView renderBySupplier(Request req, Response res) {
+        int searchedId = Integer.parseInt(req.params(":id"));
+        Map params = new HashMap<>();
+        params.put("suppliers", supplierDataStore.getAll());
+        params.put("categories", productCategoryDataStore.getAll());
+        params.put("products", productDataStore.getBy(supplierDataStore.find(searchedId)));
         return new ModelAndView(params, "product/index");
     }
 }
