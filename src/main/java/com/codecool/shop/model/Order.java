fd57f860f174516884382fc68458b419b2c9a964
pengefutar <siri.lukacs@gmail.com>
2017-05-03 11:05:58 +0200

    order status is not string but OrderStatus

--- src/main/java/com/codecool/shop/model/Order.java
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -1,35 +1,37 @@
 package com.codecool.shop.model;
 
+import com.codecool.shop.enumeration.OrderStatus;
+
 /**
  * Created by eszti on 2017.04.27..
  */
 public class Order implements Orderable{
 
     private int id;
-    private String status;
+    private OrderStatus status;
     private static int counter;
 
     public void Order(){
         this.id = counter;
         counter++;
-        this.status = "new";
+        this.status = OrderStatus.NEW;
     }
 
-    public String getStatus(){
+    public OrderStatus getStatus(){
         return this.status;
     }
 
     public boolean checkout(){
-        if (this.status=="new") {
-            this.status ="checked";
+        if (this.status==OrderStatus.NEW) {
+            this.status = OrderStatus.CHECKED;
             return true;
         }
         return false;
     }
 
     public boolean pay() {
-        if (this.status == "checked") {
-            this.status = "paid";
+        if (this.status == OrderStatus.CHECKED) {
+            this.status = OrderStatus.PAID;
             return true;
         }
         return false;
