3bb526b17a97517daa2e819c0346e3dc00158bf2
pengefutar <siri.lukacs@gmail.com>
2017-05-03 10:56:10 +0200

    models added

---
+++ src/main/java/com/codecool/shop/model/CheckoutProcess.java
@@ -0,0 +1,13 @@
+package com.codecool.shop.model;
+
+/**
+ * Created by eszti on 2017.04.27..
+ */
+public class CheckoutProcess extends AbstractProcess{
+
+    protected void action(Orderable item){
+        System.out.println("checkout process action");
+        item.checkout();
+    }
+
+}
