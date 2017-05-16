f0141379c44bb1bcdcdc5fbfc97becee95c2299b
Marti Kecskemeti <martikecskemeti@gmail.com>
2017-05-16 15:46:54 +0200

    order jdbc class added

---
+++ src/main/java/com/codecool/shop/dao/implementation/OrderDaoJdbc.java
@@ -0,0 +1,38 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.OrderDao;
+import com.codecool.shop.model.Order;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Created by marti on 2017.05.16..
+ */
+public class OrderDaoJdbc extends JdbcDao implements OrderDao {
+    @Override
+    public void add(Order order) {
+
+    }
+
+    @Override
+    public Order find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Order> getAll() {
+        return null;
+    }
+
+    @Override
+    Connection getConnection() throws SQLException {
+        return null;
+    }
+}
