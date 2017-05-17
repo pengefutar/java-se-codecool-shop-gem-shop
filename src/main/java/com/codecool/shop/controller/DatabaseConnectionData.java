e5ca9f829186c8752ece4517f3911d5f636f5fd5
keligeri <kel.gergo@gmail.com>
2017-05-17 11:50:45 +0200

    REad from config file and split it to List

--- src/main/java/com/codecool/shop/controller/DatabaseConnectionData.java
+++ src/main/java/com/codecool/shop/controller/DatabaseConnectionData.java
@@ -1,7 +1,14 @@
 package com.codecool.shop.controller;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -9,38 +16,52 @@ import java.util.Scanner;
  */
 public class DatabaseConnectionData {
 
-    private static String filePath = "parameter.txt";
-    private static final String DATABASE = "jdbc:postgresql://localhost:5432/gem_shop";
+    private static String filePath = "src/main/resources/connection.properties";
+    private static String DB_URL = "jdbc:postgresql://localhost:5432/gem_shop";
+    private static String DB_NAME;
     private static String DB_USER;
     private static String DB_PASSWORD;
 
     public static String getDATABASE() {
-        return DATABASE;
+        return DB_URL;
     }
 
-    public static String getDB_USER() {
-        getUserAndPasswordFromFile();
-        return DB_USER;
+    public static String getDB_USER(){
+        try {
+            getUserAndPasswordFromFile();
+            return DB_USER;
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return null;
     }
 
-    public static String getDB_PASSWORD() {
-        getUserAndPasswordFromFile();
+    public static String getDB_PASSWORD(){
+        try {
+            getUserAndPasswordFromFile();
+            return DB_PASSWORD;
+        } catch (IOException e){
+            e.printStackTrace();
+        }
         return DB_PASSWORD;
     }
 
-    private static void getUserAndPasswordFromFile() {
+    private static void getUserAndPasswordFromFile() throws IOException {
+        StringBuilder content = new StringBuilder();
+        List<String> allLinesList = Arrays.asList(Files.readAllLines(Paths.get(filePath)).
+                toString().split("\n"));
 
-        String content = "";
-        try {
-            content = new Scanner(new File(filePath)).useDelimiter("\\Z").next();
-        }
-        catch (IOException e) {
-            System.out.println("Please make a parameter.txt file " +
-                    "following the instructions in parameter_template.txt");
-        }
+        allLinesList.forEach(word -> content.append(word));
+        System.out.println(content);
 
-        String userAndPass[] = content.split(";");
-        DB_USER = userAndPass[0];
-        DB_PASSWORD = userAndPass[1];
+        DB_URL = allLinesList.get(0);
+        DB_NAME = allLinesList.get(1);
+        DB_USER = allLinesList.get(2);
+        DB_PASSWORD = allLinesList.get(3);
+    }
+
+    public static void main(String[] args) throws IOException {
+        DatabaseConnectionData.getUserAndPasswordFromFile();
+
     }
 }
